8ca359f6eb50ddd0d3b3090b4481cd01316a8e5c
Denys Medynskyi <denys.medynskyi@gmail.com>
2016-08-05 18:00:53 +0300

    create post after email is processed

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -1,11 +1,5 @@
 class EmailProcessor
-
-  attr_reader :email
-
-  def initialize(email)
-    @email = email
-  end
-
-  def process
+  def self.process(email)
+    Post.create!({ body: email.body, email: email.from })
   end
 end
